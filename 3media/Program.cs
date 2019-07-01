fcb1ef8a3cf708abbb508199bc7e1c3511f6da1d
David Alfageme <alfalfageme@gmail.com>
2019-07-01 13:07:51 +0200

    Media de parámetros

---
+++ 3media/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _3media
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float total = 0;
+            
+            foreach (string input in args)
+            {
+                Int32 number = Int32.Parse(input);
+                total += number;   
+            }
+
+            float resultado = total / args.Length;
+
+            Console.WriteLine("El resultado es: " + resultado);
+        }
+    }
+}
+
+// /pazcostea@gmail.com vmhgarcia2@gmail.com  nachomartinezsiles@outlook.com
