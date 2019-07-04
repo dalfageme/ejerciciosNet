0d5b4f6a67cc7d06a7599ba145ac8bec7e63a23b
David Alfageme <alfalfageme@gmail.com>
2019-07-04 08:40:43 +0200

    Clase principal forma

---
+++ formas/Forma.cs
@@ -0,0 +1,38 @@
+namespace formas
+{
+    public class Forma
+    {
+        private double perimetro;
+        private double area;
+        public Forma()
+        {
+
+        }
+
+        public Forma(double perimetro, double area)
+        {
+            this.setPerimetro(perimetro);
+            this.setArea(area);
+        }
+
+        public double getPerimetro()
+        {
+            return this.perimetro;
+        }
+
+        public void setPerimetro(double perimetro)
+        {
+            this.perimetro = perimetro;
+        }
+
+        public double getArea()
+        {
+            return this.area;
+        }
+
+        public void setArea(double area)
+        {
+            this.area = area;
+        }
+    }
+}
---
+++ formas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace formas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
