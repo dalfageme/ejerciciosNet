61103d07822aee87fcd9848d924fa9d382715a32
Alfageme <alfalfageme@gmail.com>
2019-07-04 09:06:53 +0200

    error forma

--- formas/Program.cs
+++ formas/Program.cs
@@ -6,7 +6,8 @@ namespace formas
     {
         static void Main(string[] args)
         {
-            Rectangulo f = new Forma();
+            // Esto da error
+            // Rectangulo f = new Forma();
             double b = 10;
             double h = 5;
             // Una variable tipo forma puede contener un rectangulo
